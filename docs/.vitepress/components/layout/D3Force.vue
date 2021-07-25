3d16afbc4cd91a05c89cb4dd351f0b5f6b5542ce
dash14 <dash14.ack@gmail.com>
2021-07-25 18:41:11 +0900

    Add demo of layout / positioning

---
+++ docs/.vitepress/components/layout/D3Force.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="demo-control-panel">
+    <label>Node count:</label>
+    <el-input-number v-model="nodeCount" :min="3" :max="200" />
+    <label>(&lt;= 300)</label>
+  </div>
+
+  <v-network-graph
+    :zoom-level="0.5"
+    :nodes="nodes"
+    :edges="edges"
+    :configs="configs"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, watchEffect } from "vue"
+import { Nodes, Edges } from "v-network-graph"
+import { ForceLayout } from "v-network-graph/force-layout"
+// import * as d3 from "d3-force"
+
+function buildNetwork(count: number, nodes: Nodes, edges: Edges) {
+  const idNums = [...Array(count)].map((_, i) => i)
+
+  // nodes
+  const newNodes = Object.fromEntries(idNums.map(id => [`node${id}`, {}]))
+  Object.keys(nodes).forEach(id => delete nodes[id])
+  Object.assign(nodes, newNodes)
+
+  // edges
+  const makeEdgeEntry = (id1: number, id2: number) => {
+    return [`edge${id1}-${id2}`, { source: `node${id1}`, target: `node${id2}` }]
+  }
+  const newEdges = Object.fromEntries(
+    [
+      ...idNums
+        .map(n => [n, (Math.floor(n / 5) * 5) % count])
+        .map(([n, m]) => n === m ? [n, (n + 5) % count] : [n, m])
+        .map(([n, m]) => makeEdgeEntry(n, m)),
+    ]
+  )
+  Object.keys(edges).forEach(id => delete edges[id])
+  Object.assign(edges, newEdges)
+}
+
+export default defineComponent({
+  setup() {
+    const nodeCount = ref(20)
+    const nodes = reactive({})
+    const edges = reactive({})
+
+    watchEffect(() => {
+      buildNetwork(nodeCount.value, nodes, edges)
+    })
+
+    const configs = reactive({
+      view: {
+        layoutHandler: new ForceLayout({
+          positionFixedByDrag: false,
+          positionFixedByClickWithAltKey: true,
+          // * The following are the default parameters for the simulation.
+          // * You can customize it by uncommenting below.
+          // createSimulation: (nodeLayouts, edges) => {
+          //   return d3.forceSimulation(nodeLayouts)
+          //     .force("edge", edges.distance(100))
+          //     .force("charge", d3.forceManyBody())
+          //     .force("collide", d3.forceCollide(50).strength(0.2))
+          //     .force("center", d3.forceCenter().strength(0.05))
+          //     .alphaMin(0.001)
+          // }
+        }),
+      },
+      node: {
+        label: {
+          visible: false,
+        },
+      },
+    })
+
+    return { nodeCount, nodes, edges, configs }
+  },
+})
+</script>
--- docs/examples/layout.md
+++ docs/examples/layout.md
@@ -18,12 +18,46 @@ If you set the `view.scalingObjects` config to `true`, the objects will change a
 
 ## Snap nodes to the grid
 
+You can change the node positioning strategy in the config.  
+An example of modifying the `view.layoutHandler` config and snapping to the grid is shown below.
+
+<demo-tabs message="When you drag a node, it will snap to invisible grids of the specified width.">
+<template v-slot:demo>
+  <DemoSnapToGrid />
+</template>
+<template v-slot:source>
+
+  <<< @/.vitepress/components/layout/SnapToGrid.vue{36-38}
+
+</template>
+</demo-tabs>
 
 ## Position nodes with d3-force
 
+This component also provide a layout strategy with simulating physical forces on particles with [d3-force](https://github.com/d3/d3-force).
 
+<demo-tabs :demo-height="450">
+<template v-slot:demo>
+  <DemoD3Force />
+</template>
+<template v-slot:source>
+
+  <<< @/.vitepress/components/layout/D3Force.vue{65-78}
+
+</template>
+</demo-tabs>
+
+To use this class, install the `d3-force` package with the following command.
+(Also install `@types/d3-force` if necessary.)
+
+
+```bash
+$ npm install d3-force
+```
 
 <script setup>
 import DemoScaleObjects from '../.vitepress/components/layout/ScaleObjects.vue'
+import DemoSnapToGrid from '../.vitepress/components/layout/SnapToGrid.vue'
+import DemoD3Force from '../.vitepress/components/layout/D3Force.vue'
 </script>
 
