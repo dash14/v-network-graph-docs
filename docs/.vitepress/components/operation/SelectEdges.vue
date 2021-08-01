ff1e8c283cdf403dd4a4aaf86c413ce4f4430863
dash14 <dash14.ack@gmail.com>
2021-08-01 15:33:40 +0900

    Remove dead code

--- docs/.vitepress/components/operation/SelectEdges.vue
+++ docs/.vitepress/components/operation/SelectEdges.vue
@@ -18,10 +18,10 @@
 
     <label>Limit:</label>
     <el-select v-model="limit">
-      <el-option :key="0" label="0 (disable)" :value="0" />
-      <el-option :key="1" label="1 edge" :value="1" />
-      <el-option :key="2" label="2 edges" :value="2" />
-      <el-option :key="3" label="unlimited" :value="-1" />
+      <el-option label="0 (disable)" :value="0" />
+      <el-option label="1 edge" :value="1" />
+      <el-option label="2 edges" :value="2" />
+      <el-option label="unlimited" :value="-1" />
     </el-select>
   </div>
 
@@ -36,16 +36,17 @@
 
 <script lang="ts">
 import { defineComponent, reactive, ref, watch } from "vue"
+import { Nodes, Edges } from "v-network-graph"
 import * as vNG from "v-network-graph"
 
 export default defineComponent({
   setup() {
-    const nodes = {
+    const nodes: Nodes = {
       node1: { name: "N1" },
       node2: { name: "N2" },
       node3: { name: "N3" },
     }
-    const edges = {
+    const edges: Edges = {
       edge1: { source: "node1", target: "node2" },
       edge2: { source: "node2", target: "node3" },
       edge3: { source: "node3", target: "node1" },
