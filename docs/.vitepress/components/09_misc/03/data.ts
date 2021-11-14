3b380cb2c423ce35fe0824fa4edbd87e00de1e14
dash14 <dash14.ack@gmail.com>
2021-11-14 16:18:49 +0900

    Add a example for finding shortest path

---
+++ docs/.vitepress/components/09_misc/03/data.ts
@@ -0,0 +1,74 @@
+import { Nodes, Edges, Layouts, UserConfigs } from "v-network-graph"
+
+const nodes: Nodes = {
+  node1: { name: "Source" },
+  node2: { name: "" },
+  node3: { name: "" },
+  node4: { name: "" },
+  node5: { name: "" },
+  node6: { name: "" },
+  node7: { name: "" },
+  node8: { name: "" },
+  node9: { name: "" },
+  node10: { name: "" },
+  node11: { name: "" },
+  node12: { name: "" },
+}
+
+const edges: Edges = {
+  edge1: { source: "node1", target: "node2", cost: 10 },
+  edge2: { source: "node1", target: "node3", cost: 5 },
+  edge3: { source: "node2", target: "node4", cost: 10 },
+  edge4: { source: "node2", target: "node5", cost: 10 },
+  edge5: { source: "node3", target: "node5", cost: 5 },
+  edge6: { source: "node3", target: "node6", cost: 10 },
+  edge7: { source: "node4", target: "node7", cost: 10 },
+  edge8: { source: "node5", target: "node8", cost: 5 },
+  edge9: { source: "node6", target: "node9", cost: 10 },
+  edge10: { source: "node7", target: "node10", cost: 5 },
+  edge11: { source: "node8", target: "node10", cost: 5 },
+  edge12: { source: "node8", target: "node11", cost: 10 },
+  edge13: { source: "node9", target: "node11", cost: 10 },
+  edge14: { source: "node10", target: "node12", cost: 10 },
+  edge15: { source: "node11", target: "node12", cost: 10 },
+}
+
+const layouts: Layouts = {
+  nodes: {
+    node1: { x: 0, y: 100 },
+    node2: { x: 75, y: 50 },
+    node3: { x: 75, y: 150 },
+    node4: { x: 150, y: 0 },
+    node5: { x: 150, y: 100 },
+    node6: { x: 150, y: 200 },
+    node7: { x: 250, y: 0 },
+    node8: { x: 250, y: 100 },
+    node9: { x: 250, y: 200 },
+    node10: { x: 325, y: 50 },
+    node11: { x: 325, y: 150 },
+    node12: { x: 400, y: 100 },
+  },
+}
+
+const configs: UserConfigs = {
+  node: {
+    label: {
+      visible: (n) => !!n.name
+    }
+  },
+  path: {
+    visible: true,
+    curveInNode: true,
+    path: {
+      width: 14,
+      color: "#ff000088"
+    }
+  }
+}
+
+export default {
+  nodes,
+  edges,
+  layouts,
+  configs,
+}
--- docs/examples/misc.md
+++ docs/examples/misc.md
@@ -39,9 +39,39 @@ In this example, the SVG data is downloaded.
 </template>
 </demo-tabs>
 
+## Find the shortest path
+
+v-network-graph is intended for drawing graphs, and does not
+contain any code for graph algorithms.
+On the other hand, it can be used with any graph algorithm.  
+The following is an example of drawing the shortest path using
+Dijkstra's algorithm.
+The edge label indicates the cost of that edge.
+v-network-graph's path drawing is directed by a list of edges,
+so it is converted from a list of nodes to a list of edges at
+the end.
+
+<demo-tabs :demo-height="350" :use-data="true" hint="Displays the shortest path from the 'Source' to the mouse-over node.">
+<template v-slot:demo>
+  <DemoDijkstra />
+</template>
+<template v-slot:source>
+
+  <<< @/.vitepress/components/09_misc/03/Dijkstra.vue
+
+</template>
+<template v-slot:data>
+
+  <<< @/.vitepress/components/09_misc/03/data.ts
+
+</template>
+</demo-tabs>
+
 
 <script setup>
 import DemoDownload from '../.vitepress/components/09_misc/01/Download.vue'
 import DemoGridLayer from '../.vitepress/components/09_misc/02/GridLayer.vue'
+import DemoDijkstra from '../.vitepress/components/09_misc/03/Dijkstra.vue'
+
 </script>
 
