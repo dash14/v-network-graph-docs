d2bf7e7bc3c1f4141686af3d677820e23e4b79bf
dash14 <dash14.ack@gmail.com>
2022-06-18 22:22:35 +0900

    Add an example of placing any object at the edge

---
+++ docs/.vitepress/components/05_appearance/10/PlaceObjectAtEdge.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import * as vNG from "v-network-graph"
+import data from "./data"
+
+const configs = vNG.defineConfigs({
+  node: {
+    selectable: true,
+  },
+  edge: {
+    selectable: true,
+    normal: { color: "#5555dd" },
+    hover: { color: "#dd5555" },
+    selected: { color: "#dddd55" },
+    gap: 10,
+  },
+})
+
+/**
+ * Make `transform` value of the object placing on the edge.
+ * @param area - label area calculated by v-network-graph
+ * @param scale - zooming scale
+ */
+function makeTransform(area: vNG.EdgeLabelArea, scale: number) {
+  const x = (area.source.above.x + area.target.below.x) / 2
+  const y = (area.source.above.y + area.target.below.y) / 2
+  const radian = Math.atan2(
+    area.target.above.y - area.source.above.y,
+    area.target.above.x - area.source.above.x
+  )
+  const degree = (radian * 180.0) / Math.PI
+
+  return [
+    `translate(${x} ${y})`,
+    `scale(${scale}, ${scale})`,
+    `rotate(${degree})`,
+  ].join(" ")
+}
+</script>
+
+<template>
+  <v-network-graph
+    :nodes="data.nodes"
+    :edges="data.edges"
+    :layouts="data.layouts"
+    :configs="configs"
+  >
+    <!-- To use CSS within SVG, use <defs>. -->
+    <defs>
+      <!-- Cannot use <style> directly due to restrictions of Vue. -->
+      <component is="style">
+        <!-- prettier-ignore -->
+        .marker {
+          fill: {{ configs.edge.normal.color }};
+          transition: fill 0.1s linear;
+        }
+        .marker.hovered { fill: {{ configs.edge.hover.color }}; }
+        .marker.selected { fill: {{ configs.edge.selected.color }}; }
+      </component>
+    </defs>
+    <template #edge-label="{ scale, area, hovered, selected }">
+      <!-- Triangle -->
+      <path
+        class="marker"
+        :class="{ hovered, selected }"
+        d="M-5 -5 L5 0 L-5 5 Z"
+        :transform="makeTransform(area, scale)"
+      />
+    </template>
+  </v-network-graph>
+</template>
--- docs/.vitepress/components/05_appearance/10/data.ts
+++ docs/.vitepress/components/05_appearance/10/data.ts
@@ -1,19 +1,24 @@
 import { Nodes, Edges, Layouts } from "v-network-graph"
 
 const nodes: Nodes = {
+  node0: { name: "N0" },
   node1: { name: "N1" },
   node2: { name: "N2" },
 }
 
 const edges: Edges = {
-  edge1: { source: "node1", target: "node2", color: "red" },
-  edge2: { source: "node2", target: "node1", color: "green" },
+  edge1: { id: "edge1", source: "node0", target: "node1" },
+  edge2: { id: "edge2", source: "node1", target: "node2" },
+  edge3: { id: "edge3", source: "node2", target: "node1" },
+  edge4: { id: "edge4", source: "node2", target: "node3" },
+  edge5: { id: "edge5", source: "node0", target: "node2" },
 }
 
 const layouts: Layouts = {
   nodes: {
-    node1: { x: 0, y: 0 },
-    node2: { x: 80, y: 80 },
+    node0: { x: 80, y: 0 },
+    node1: { x: 0, y: 120 },
+    node2: { x: 160, y: 120 },
   },
 }
 
