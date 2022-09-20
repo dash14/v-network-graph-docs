6c2340dbefe3b8579a8cb3c288ebf362e768fd90
dash14 <dash14.ack@gmail.com>
2022-09-20 23:10:59 +0900

    Fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,12 +24,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? 2 : 0,
     "vue/singleline-html-element-content-newline": 0,
     "vue/multiline-html-element-content-newline": 0,
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": {
-        "max": 3,
-        "allowFirstLine": true
-      }
-    }],
+    "vue/max-attributes-per-line": ["error", { "singleline": 3 }],
     // "no-unused-vars": ["warn", { args: "after-used", argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
     "@typescript-eslint/explicit-module-boundary-types": 0,
     "@typescript-eslint/no-explicit-any": 0,
