b3fd0f06a581ef295919b3ae4af6bf22ad651f45
dash14 <dash14.ack@gmail.com>
2022-11-20 22:51:10 +0900

    Fix eslint settings and add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:import/recommended",
     "plugin:vue/base",
     "plugin:vue/vue3-recommended",
     "plugin:@typescript-eslint/recommended",
@@ -18,18 +19,30 @@ module.exports = {
   plugins: [
     "vue",
     "@typescript-eslint",
+    "import",
     "prettier"
   ],
   rules: {
+    "import/order": [
+      "error",
+      { groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object"] },
+    ],
     "no-console": process.env.NODE_ENV === "production" ? 2 : 0,
     "vue/singleline-html-element-content-newline": 0,
     "vue/multiline-html-element-content-newline": 0,
-    "vue/max-attributes-per-line": ["error", { "singleline": 3 }],
-    // "no-unused-vars": ["warn", { args: "after-used", argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    "vue/max-attributes-per-line": ["error", { singleline: 3, multiline: 1 }],
+    "vue/attribute-hyphenation": ["warn", "always", { ignore: ["custom-prop"] }],
     "@typescript-eslint/explicit-module-boundary-types": 0,
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-empty-function": 0,
     "no-unused-vars": 0,
-    "@typescript-eslint/no-unused-vars": ["warn", { args: "after-used", argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { args: "after-used", argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
   },
+  settings: {
+    "import/resolver": { typescript: [] },
+  },
+  "ignorePatterns": ["!.vitepress"]
 }
